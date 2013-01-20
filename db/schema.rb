47abdc638b9a3b99d48b4e855639f5f74be82edb
koshikawa <koshikawa@ppworks.jp>
2013-01-20 14:42:19 +0900

    [#42848797] Add User::Guest model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130120052600) do
+ActiveRecord::Schema.define(:version => 20130120060859) do
 
   create_table "chats", :force => true do |t|
     t.integer  "room_id",    :null => false
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(:version => 20130120052600) do
     t.datetime "created_at",                                    :null => false
     t.datetime "updated_at",                                    :null => false
     t.string   "color",                  :default => "#000000", :null => false
+    t.string   "type"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
