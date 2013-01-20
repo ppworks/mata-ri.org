327a1626d5130f2d0f16bd782fc23d77a7704eee
koshikawa <koshikawa@ppworks.jp>
2013-01-20 15:43:41 +0900

    [#42848797] Fix spec

--- spec/controllers/chats_controller_spec.rb
+++ spec/controllers/chats_controller_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe ChatsController do
   let(:room) { FactoryGirl.create(:room1) } 
   let(:chat) { FactoryGirl.create(:chat, room_id: room.id) } 
+  let(:user) { FactoryGirl.create(:user) }
   
   subject { response }
   describe "get 'index'" do
     before do
+      sign_in user
       get :index, room_id: room.id
     end
     it { should be_success }
@@ -14,6 +16,7 @@ describe ChatsController do
 
   describe "get 'show'" do
     before do
+      sign_in user
       get :show, room_id: room.id, id: chat.id
     end
     it { should be_success }
@@ -24,6 +27,7 @@ describe ChatsController do
     let(:user_name) { 'test' }
     let(:content) { 'test' }
     before do
+      sign_in user
       post :create, room_id: room.id, user_name: user_name, color: color, content: content
     end
     it { should redirect_to room_chats_path(room) }
--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe ParticipantsController do
   let(:room) { FactoryGirl.create(:room1) } 
+  let(:user) { FactoryGirl.create(:user) }
   
   subject { response }
   describe "POST 'create'" do
@@ -25,20 +26,11 @@ describe ParticipantsController do
 
   describe "DELETE 'destroy'" do
     let(:color) { '#333333' }
-    context 'no name given' do
-      let(:user_name) { '' }
-      before do
-        delete :destroy, room_id: room.id, user_name: user_name, color: color
-      end
-      it { should redirect_to room_path(room) }
+    let(:user_name) { 'test' }
+    before do
+      sign_in user
+      delete :destroy, room_id: room.id, user_name: user_name, color: color
     end
-
-    context 'valid name given' do
-      let(:user_name) { 'test' }
-      before do
-        delete :destroy, room_id: room.id, user_name: user_name, color: color
-      end
-      it { should be_success }
-    end
+    it { should redirect_to root_path }
   end
 end
