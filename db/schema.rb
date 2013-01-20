623b3b96ebbddd2085a215d9f1ee25c7d60e298b
koshikawa <koshikawa@ppworks.jp>
2013-01-20 14:41:53 +0900

    [#42848797] Add users.color column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130117142711) do
+ActiveRecord::Schema.define(:version => 20130120052600) do
 
   create_table "chats", :force => true do |t|
     t.integer  "room_id",    :null => false
@@ -64,8 +64,8 @@ ActiveRecord::Schema.define(:version => 20130117142711) do
   create_table "users", :force => true do |t|
     t.string   "name",                   :default => ""
     t.string   "image",                  :default => ""
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",        :null => false
+    t.string   "encrypted_password",     :default => "",        :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -74,8 +74,9 @@ ActiveRecord::Schema.define(:version => 20130117142711) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                    :null => false
+    t.datetime "updated_at",                                    :null => false
+    t.string   "color",                  :default => "#000000", :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
