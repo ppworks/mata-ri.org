2f0bc2ec73775655d063fbc985631acb7edb8273
koshikawa <koshikawa@ppworks.jp>
2013-01-26 00:20:00 +0900

    [#43217343] Remove Chat::Arrived & Chat::Left '.content'

--- app/models/chat/arrived.rb
+++ app/models/chat/arrived.rb
@@ -1,13 +1,8 @@
 class Chat::Arrived < Chat
-  after_initialize :set_default_values
   after_create :send_other_rooms
   after_create :update_room
 
   private
-  def set_default_values
-    self.content = I18n.t('chat.arrived.content')
-  end
-
   def update_room
     return unless self.origin_room_id == self.room_id
     self.room.update_column(:last_arrived_at, self.created_at)
--- app/models/chat/left.rb
+++ app/models/chat/left.rb
@@ -1,13 +1,8 @@
 class Chat::Left < Chat
-  after_initialize :set_default_values
   after_create :send_other_rooms
   after_create :update_room
 
   private
-  def set_default_values
-    self.content = I18n.t('chat.left.content')
-  end
-
   def update_room
     return unless self.origin_room_id == self.room_id
     self.room.update_column(:last_left_at, self.created_at)
