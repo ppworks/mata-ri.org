a33b4fab85c987e263dd838de258b03d370b6ae4
koshikawa <koshikawa@ppworks.jp>
2013-01-27 22:04:39 +0900

    [#42928741] Add ROM feature

--- app/controllers/pushers_controller.rb
+++ app/controllers/pushers_controller.rb
@@ -4,8 +4,13 @@ class PushersController < ApplicationController
   def authentication
     res = Pusher[params[:channel_name]].authenticate(
       params[:socket_id],
-      user_id: current_user.id,
-      user_info: current_user
+      user_id: current_user.try(:id) || Digest::SHA1.hexdigest(Time.current.to_f.to_s),
+      user_info: {
+        id: current_user.try(:id),
+        name: current_user.try(:name),
+        color: current_user.try(:color),
+        path: request.referer,
+      },
     )
     render json: res
   end
