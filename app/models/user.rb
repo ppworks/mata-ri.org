8651c8f81cd528acc4586df1b4db31a7302b0dd8
koshikawa <koshikawa@ppworks.jp>
2013-01-28 12:39:05 +0900

    [#42928803] Add the feature that user can enter chat with facebook account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class User < ActiveRecord::Base
   include Providers::Facebook
   devise :database_authenticatable, :registerable,
@@ -5,7 +6,23 @@ class User < ActiveRecord::Base
          :omniauthable
 
   attr_accessible :color, :email, :image, :name, :password, :password_confirmation, :remember_me
+  
+  validate :name_is_valid
+  validates_presence_of :name
 
-  has_many :connections
+  has_many :connections, dependent: :delete_all
   has_many :providers, through: :connections
+
+
+  def nick_name
+    name.sub(/@|＠.*/, '')
+  end
+
+  private
+  def name_is_valid
+    user = User::Reserved.where(name: self.nick_name).first
+    if user.present? && user.id != self.id
+      errors.add(:name, I18n.t('activerecord.errors.models.user.attributes.name.reserved'))
+    end
+  end
 end
